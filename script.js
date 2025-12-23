5a415b4ff911561fa23ec641d9dba9d4564f99e5
ChopsDev <101037256+ChopsDev@users.noreply.github.com>
2025-12-23 22:47:33 +1300

    Better Humming

--- script.js
+++ script.js
@@ -1334,38 +1334,76 @@ function createCRTHum() {
   crtGainNode.gain.value = 0;
   crtGainNode.connect(crtAudioContext.destination);
 
-  // 60Hz power hum
-  const hum60 = crtAudioContext.createOscillator();
-  hum60.type = 'sine';
-  hum60.frequency.value = 60;
-  const gain60 = crtAudioContext.createGain();
-  gain60.gain.value = 0.04;
-  hum60.connect(gain60);
-  gain60.connect(crtGainNode);
-  hum60.start();
-  crtOscillators.push(hum60);
+  // Low-pass filter to soften the hum
+  const humFilter = crtAudioContext.createBiquadFilter();
+  humFilter.type = 'lowpass';
+  humFilter.frequency.value = 350;
+  humFilter.Q.value = 0.7;
+  humFilter.connect(crtGainNode);
 
-  // 120Hz harmonic
-  const hum120 = crtAudioContext.createOscillator();
-  hum120.type = 'sine';
-  hum120.frequency.value = 120;
-  const gain120 = crtAudioContext.createGain();
-  gain120.gain.value = 0.02;
-  hum120.connect(gain120);
-  gain120.connect(crtGainNode);
-  hum120.start();
-  crtOscillators.push(hum120);
+  // LFO for subtle wobble effect
+  const lfo = crtAudioContext.createOscillator();
+  const lfoGain = crtAudioContext.createGain();
+  lfo.type = 'sine';
+  lfo.frequency.value = 0.3; // Slow wobble
+  lfoGain.gain.value = 3; // Subtle frequency variation
+  lfo.connect(lfoGain);
+  lfo.start();
+  crtOscillators.push(lfo);
 
-  // High-pitched flyback whine (~15.7kHz)
-  const flyback = crtAudioContext.createOscillator();
-  flyback.type = 'sine';
-  flyback.frequency.value = 15700;
-  const gainFlyback = crtAudioContext.createGain();
-  gainFlyback.gain.value = 0.008;
-  flyback.connect(gainFlyback);
-  flyback.connect(crtGainNode);
-  flyback.start();
-  crtOscillators.push(flyback);
+  // 100Hz base hum - warmer frequency
+  const hum100 = crtAudioContext.createOscillator();
+  hum100.type = 'sine';
+  hum100.frequency.value = 100;
+  lfoGain.connect(hum100.frequency); // Add wobble
+  const gain100 = crtAudioContext.createGain();
+  gain100.gain.value = 0.18;
+  hum100.connect(gain100);
+  gain100.connect(humFilter);
+  hum100.start();
+  crtOscillators.push(hum100);
+
+  // 200Hz harmonic - adds body
+  const hum200 = crtAudioContext.createOscillator();
+  hum200.type = 'sine';
+  hum200.frequency.value = 200;
+  const gain200 = crtAudioContext.createGain();
+  gain200.gain.value = 0.1;
+  hum200.connect(gain200);
+  gain200.connect(humFilter);
+  hum200.start();
+  crtOscillators.push(hum200);
+
+  // 50Hz sub bass - feels more like a real CRT
+  const hum50 = crtAudioContext.createOscillator();
+  hum50.type = 'sine';
+  hum50.frequency.value = 50;
+  const gain50 = crtAudioContext.createGain();
+  gain50.gain.value = 0.12;
+  hum50.connect(gain50);
+  gain50.connect(humFilter);
+  hum50.start();
+  crtOscillators.push(hum50);
+
+  // 150Hz mid tone for fullness
+  const hum150 = crtAudioContext.createOscillator();
+  hum150.type = 'sine';
+  hum150.frequency.value = 150;
+  const lfo2 = crtAudioContext.createOscillator();
+  const lfo2Gain = crtAudioContext.createGain();
+  lfo2.type = 'sine';
+  lfo2.frequency.value = 0.5;
+  lfo2Gain.gain.value = 2;
+  lfo2.connect(lfo2Gain);
+  lfo2Gain.connect(hum150.frequency);
+  lfo2.start();
+  crtOscillators.push(lfo2);
+  const gain150 = crtAudioContext.createGain();
+  gain150.gain.value = 0.08;
+  hum150.connect(gain150);
+  gain150.connect(humFilter);
+  hum150.start();
+  crtOscillators.push(hum150);
 }
 
 function toggleCRTEffect(enable) {
